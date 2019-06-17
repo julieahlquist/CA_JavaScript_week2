bb42b8fc08c3ca0738048225e804802ce84b2a5f
Julie Ahlquist <julieahlquist@hotmail.com>
2019-06-18 00:03:03 +0200

    added text file with answers to js exercises

---
+++ exercises.js
@@ -0,0 +1,35 @@
+// Exercise 1
+
+let numberArray = [ 3, 24, 72 ]
+
+let number = numberArray[1]
+
+123/12 is a number, "strings", variables that doesn't have an assigned value are undefined
+
+let myArray = [ 'Thomas', 'Amber', 'Raoul' ]
+let emptyArray = []
+for (i = 0; i < people.length; i++) {
+    myArray.push(people[i])
+}
+
+// Exercise 2
+this is the 'owner' of the method, eqvivalent to self in ruby
+
+class Person {
+    constructor (first, last) { 
+        this.first = first;        
+        this.last = last;
+    }
+
+    this.setName = function (first, last){ 
+        this.first = first;
+        this.last = last;
+    }
+}
+
+// Async/await
+putting async function redirects Javascript to always return 
+a Promise, which is advantage is that its 'safer'. An asynchronous
+function can be paused by await, so that the promise is returned
+when whatever the machine is running meanwhile, is completed, 
+which is favorable since it doesn't 'cost' any resources. 
