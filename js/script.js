3b7fada143fe5f3583ba48cab122f138c4b65cd9
Julie Ahlquist <julieahlquist@hotmail.com>
2019-06-17 14:21:06 +0200

    first commits for new submission

---
+++ js/script.js
@@ -0,0 +1,25 @@
+document.write("js/script.js");
+
+let date, joke;
+
+let array_jokes = [
+    "Can I take your temperature? You’re looking hot today.", 
+    "Are you my appendix? I have a gut feeling I should take you out.",
+    "Can I be your ophthalmologist ‘cause I can’t stop looking into your eyes.", 
+    "You’d better be a cardiologist, because something about you makes me want to give you my heart."
+];
+
+let randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
+
+let showJoke = document.getElementById('showjoke');
+displayJoke.innerHTML = randomJoke
+}
+
+date = "Today";
+
+// Code for showing today's date when pressing the first button
+const date: Date = DateUtils.overrideTimezone(someDateStringFromServer);
+
+// Code for showing a random joke picked from an array of jokes
+
+// gives a collection of elements
